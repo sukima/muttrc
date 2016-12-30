01aea720bcd7d74c7f5793aa1fe61941e88318fa
Devin Weaver <suki@tritarget.org>
2016-12-30 18:01:46 -0500

    Allow selecting from multiple accounts

--- home/.mutt/load_account_settings.sh
+++ home/.mutt/load_account_settings.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-exec gpg --use-agent --no-tty -q -d ~/.secure/mutt_account.gpg
+DIR=~/.secure/mutt_account.d
+pushd $DIR >/dev/null
+
+echo "Load which mutt account: (CTRL-C to cancel)" 1>&2
+
+select file in *; do
+  popd >/dev/null
+  if [[ $file == *.gpg ]]; then
+    exec gpg --use-agent --no-tty -q -d "${DIR}/${file}"
+  else
+    exec cat "$DIR/${file}"
+  fi
+done
